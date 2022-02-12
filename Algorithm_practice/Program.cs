9c12a33c6463792edea7250266746d2c588cd33d
Le Tan Trong <trong.le@enlabsoftware.com>
2022-02-12 09:30:26 +0700

    Add project files.

---
+++ Algorithm_practice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algorithm_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Bubble Sort Algorithm/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Bubble_Sort_Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 78, 55, 45, 98, 13 };
+
+            int temp;
+
+            for(int j = 0; j < arr.Length; j++)
+            {
+                Console.WriteLine(j) ;
+
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("Sorted");
+            foreach (int item in arr)
+            {
+                Console.WriteLine(item + " ");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
